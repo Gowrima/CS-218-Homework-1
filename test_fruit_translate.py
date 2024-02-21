2c82c77ee646e63822f708950104c37faa23ac60
Gowrima <gowrima.kikkerijayaramu@sjsu.edu>
2024-02-21 14:13:18 -0800

    Update test_fruit_translate.py

--- test_fruit_translate.py
+++ test_fruit_translate.py
@@ -3,18 +3,18 @@ from fruits_unicode import *
 
 def test_translate_fruit_Kannada():
 	expected_ = 'Translation of Apple in Kannada is ಸೇಬು'
-	assert expected_ == translate_("Kannada", "Apple")
+	assert expected_ == translate_("Kannada", "Apple").decode('utf-8')
 
 def test_translate_fruit_Tamil():
 	expected_ = 'Translation of Apple in Tamil is ஆட்ப்ள்'
 
-	assert expected_ == translate_("Tamil", "Apple")
+	assert expected_ == translate_("Tamil", "Apple").decode('utf-8')
 
 
 def test_translate_fruit_Malayalam():
 	expected_ = 'Translation of Apple in Malayalam is ആപ്പിൽ'
 
-	assert expected_ == translate_("Malayalam", "Apple")
+	assert expected_ == translate_("Malayalam", "Apple").decode('utf-8')
 
 
 def test_bad_input():
